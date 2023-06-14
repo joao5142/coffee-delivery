5d08ce34dca9b48d50013109138727a4cc455340
joaopaulofnmvc <103937437+joaopaulofnmvc@users.noreply.github.com>
2023-06-14 20:15:32 -0300

    refactor:removendo consoles

--- src/pages/Checkout/components/Form/index.tsx
+++ src/pages/Checkout/components/Form/index.tsx
@@ -21,147 +21,185 @@ import { useContextSelector } from "use-context-selector";
 import { OrderContext } from "@/contexts/OrderContext";
 
 const FormAddressSchema = zod.object({
-	zipCode: zod.string().min(1, "Preencha o Campo"),
-	street: zod.string().min(1, "Preencha o Campo"),
-	number: zod.number({ invalid_type_error: "Informe um número válido" }),
-	complement: zod.string().min(1, "Preencha o Campo"),
-	neighborhood: zod.string().min(1, "Preencha o Campo"),
-	city: zod.string().min(1, "Preencha o Campo"),
-	federatedUnit: zod.string().min(1, "Preencha o Campo"),
+  zipCode: zod.string().min(1, "Preencha o Campo"),
+  street: zod.string().min(1, "Preencha o Campo"),
+  number: zod.number({ invalid_type_error: "Informe um número válido" }),
+  complement: zod.string().min(1, "Preencha o Campo"),
+  neighborhood: zod.string().min(1, "Preencha o Campo"),
+  city: zod.string().min(1, "Preencha o Campo"),
+  federatedUnit: zod.string().min(1, "Preencha o Campo"),
 });
 
 export type FormAddressSchemaType = zod.infer<typeof FormAddressSchema>;
 
 interface FormProps extends HtmlHTMLAttributes<HTMLFormElement> {
-	onFormSubmit: (data: FormAddressSchemaType) => void;
+  onFormSubmit: (data: FormAddressSchemaType) => void;
 }
 interface IFederatedUnits {
-	id: number;
-	nome: string;
-	sigla: string;
+  id: number;
+  nome: string;
+  sigla: string;
 }
 export function Form({ onFormSubmit, ...rest }: FormProps) {
-	const [federatedUnits, setFederatedUnits] = useState<IFederatedUnits[]>([]);
-	const [townships, setTownships] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
+  const [federatedUnits, setFederatedUnits] = useState<IFederatedUnits[]>([]);
+  const [townships, setTownships] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-	const address = useContextSelector(OrderContext, (context) => {
-		return context.address;
-	});
+  const address = useContextSelector(OrderContext, (context) => {
+    return context.address;
+  });
 
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		watch,
-	} = useForm<FormAddressSchemaType>({
-		defaultValues: { ...address },
-		resolver: zodResolver(FormAddressSchema),
-	});
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<FormAddressSchemaType>({
+    defaultValues: { ...address },
+    resolver: zodResolver(FormAddressSchema),
+  });
 
-	const changeAddress = useContextSelector(OrderContext, (context) => {
-		return context.setAddress;
-	});
+  const changeAddress = useContextSelector(OrderContext, (context) => {
+    return context.setAddress;
+  });
 
-	const federatedUnitSelected = watch("federatedUnit");
-	const city = watch("city");
+  const federatedUnitSelected = watch("federatedUnit");
+  const city = watch("city");
 
-	async function handleFormSubmit(data: FormAddressSchemaType) {
-		await changeAddress(data);
-		onFormSubmit(data);
-	}
-	async function fetchFederatedUnits() {
-		const response = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados");
-		const federatedUnits = await response.json();
+  async function handleFormSubmit(data: FormAddressSchemaType) {
+    await changeAddress(data);
+    onFormSubmit(data);
+  }
+  async function fetchFederatedUnits() {
+    const response = await fetch(
+      "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+    );
+    const federatedUnits = await response.json();
 
-		setFederatedUnits(federatedUnits);
-	}
-	async function fetchTownshipsByFederatedUnits() {
-		setIsLoading(true);
-		try {
-			const response = await fetch(
-				`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${federatedUnitSelected}/municipios`
-			);
-			const townships = await response.json();
+    setFederatedUnits(federatedUnits);
+  }
+  async function fetchTownshipsByFederatedUnits() {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${federatedUnitSelected}/municipios`
+      );
+      const townships = await response.json();
 
-			console.log(townships);
-			setTownships(townships);
-		} catch (error) {
-			console.log(error);
-		}
+      setTownships(townships);
+    } catch (error) {
+      console.log(error);
+    }
 
-		setIsLoading(false);
-	}
+    setIsLoading(false);
+  }
 
-	useEffect(() => {
-		fetchFederatedUnits();
-	}, []);
+  useEffect(() => {
+    fetchFederatedUnits();
+  }, []);
 
-	useEffect(() => {
-		if (federatedUnitSelected) {
-			fetchTownshipsByFederatedUnits();
-		}
-	}, [federatedUnitSelected]);
+  useEffect(() => {
+    if (federatedUnitSelected) {
+      fetchTownshipsByFederatedUnits();
+    }
+  }, [federatedUnitSelected]);
 
-	return (
-		<FormContainer {...rest} onSubmit={handleSubmit(handleFormSubmit)}>
-			{isLoading && <Loading />}
+  return (
+    <FormContainer {...rest} onSubmit={handleSubmit(handleFormSubmit)}>
+      {isLoading && <Loading />}
 
-			<Row marginTop="3.5rem" wrapRow={true} data-row>
-				<Col flexBasis="300px" data-col>
-					<Input wFull placeholder="CEP" {...register("zipCode")} />
-					{errors.zipCode && <ErrorMessage marginTop="0.4rem">{errors.zipCode.message}</ErrorMessage>}
-				</Col>
-			</Row>
-			<Row marginTop="1rem" wrapRow={true} data-row>
-				<Col flex="1" data-col>
-					<Input wFull placeholder="Rua" {...register("street")} />
-					{errors.street && <ErrorMessage marginTop="0.4rem">{errors.street.message}</ErrorMessage>}
-				</Col>
-			</Row>
-			<Row marginTop="1rem" gap={12} wrapRow={true} data-row>
-				<Col flex="4" data-col>
-					<Input wFull placeholder="Número" {...register("number", { valueAsNumber: true })} />
-					{errors.number && <ErrorMessage marginTop="0.4rem">{errors.number.message}</ErrorMessage>}
-				</Col>
-				<Col flex="10" data-col>
-					<Input wFull placeholder="Complemento" {...register("complement")} />
-					{errors.complement && <ErrorMessage marginTop="0.4rem">{errors.complement.message}</ErrorMessage>}
-				</Col>
-			</Row>
+      <Row marginTop="3.5rem" wrapRow={true} data-row>
+        <Col flexBasis="300px" data-col>
+          <Input wFull placeholder="CEP" {...register("zipCode")} />
+          {errors.zipCode && (
+            <ErrorMessage marginTop="0.4rem">
+              {errors.zipCode.message}
+            </ErrorMessage>
+          )}
+        </Col>
+      </Row>
+      <Row marginTop="1rem" wrapRow={true} data-row>
+        <Col flex="1" data-col>
+          <Input wFull placeholder="Rua" {...register("street")} />
+          {errors.street && (
+            <ErrorMessage marginTop="0.4rem">
+              {errors.street.message}
+            </ErrorMessage>
+          )}
+        </Col>
+      </Row>
+      <Row marginTop="1rem" gap={12} wrapRow={true} data-row>
+        <Col flex="4" data-col>
+          <Input
+            wFull
+            placeholder="Número"
+            {...register("number", { valueAsNumber: true })}
+          />
+          {errors.number && (
+            <ErrorMessage marginTop="0.4rem">
+              {errors.number.message}
+            </ErrorMessage>
+          )}
+        </Col>
+        <Col flex="10" data-col>
+          <Input wFull placeholder="Complemento" {...register("complement")} />
+          {errors.complement && (
+            <ErrorMessage marginTop="0.4rem">
+              {errors.complement.message}
+            </ErrorMessage>
+          )}
+        </Col>
+      </Row>
 
-			<Row marginTop="1rem" gap={12} wrapRow={true} data-row>
-				<Col flex="4" data-col>
-					<Input wFull placeholder="Bairro" {...register("neighborhood")} />
-					{errors.neighborhood && <ErrorMessage marginTop="0.4rem">{errors.neighborhood.message}</ErrorMessage>}
-				</Col>
-				<Col flex="8" data-col>
-					<Select wFull {...register("city")} value={city}>
-						<option value="">Cidade (Selecione a UF)</option>
-						{townships.map((township: { nome: string; id: string }) => {
-							return (
-								<option key={township.id} value={township.nome}>
-									{township.nome}
-								</option>
-							);
-						})}
-					</Select>
-					{errors.city && <ErrorMessage marginTop="0.4rem">{errors.city.message}</ErrorMessage>}
-				</Col>
-				<Col flex="2" data-col>
-					<Select wFull {...register("federatedUnit")} value={federatedUnitSelected}>
-						<option value="">UF</option>
-						{federatedUnits.map((fu) => {
-							return (
-								<option key={fu.id} value={fu.sigla}>
-									{fu.sigla}
-								</option>
-							);
-						})}
-					</Select>
-					{errors.federatedUnit && <ErrorMessage marginTop="0.4rem"> {errors.federatedUnit.message}</ErrorMessage>}
-				</Col>
-			</Row>
-		</FormContainer>
-	);
+      <Row marginTop="1rem" gap={12} wrapRow={true} data-row>
+        <Col flex="4" data-col>
+          <Input wFull placeholder="Bairro" {...register("neighborhood")} />
+          {errors.neighborhood && (
+            <ErrorMessage marginTop="0.4rem">
+              {errors.neighborhood.message}
+            </ErrorMessage>
+          )}
+        </Col>
+        <Col flex="8" data-col>
+          <Select wFull {...register("city")} value={city}>
+            <option value="">Cidade (Selecione a UF)</option>
+            {townships.map((township: { nome: string; id: string }) => {
+              return (
+                <option key={township.id} value={township.nome}>
+                  {township.nome}
+                </option>
+              );
+            })}
+          </Select>
+          {errors.city && (
+            <ErrorMessage marginTop="0.4rem">
+              {errors.city.message}
+            </ErrorMessage>
+          )}
+        </Col>
+        <Col flex="2" data-col>
+          <Select
+            wFull
+            {...register("federatedUnit")}
+            value={federatedUnitSelected}
+          >
+            <option value="">UF</option>
+            {federatedUnits.map((fu) => {
+              return (
+                <option key={fu.id} value={fu.sigla}>
+                  {fu.sigla}
+                </option>
+              );
+            })}
+          </Select>
+          {errors.federatedUnit && (
+            <ErrorMessage marginTop="0.4rem">
+              {" "}
+              {errors.federatedUnit.message}
+            </ErrorMessage>
+          )}
+        </Col>
+      </Row>
+    </FormContainer>
+  );
 }
--- src/reducers/order/reducer.ts
+++ src/reducers/order/reducer.ts
@@ -4,96 +4,100 @@ import { Alert } from "@/utils/Alert";
 import { setOrderToStorage } from "@/storage/order/setOrder";
 
 export interface ICoffee {
-	id: number;
-	name: string;
-	description: string;
-	price: number;
-	tags: string[];
-	image: string;
-	quantity?: number;
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  tags: string[];
+  image: string;
+  quantity?: number;
 }
 
 export interface IAddress {
-	zipCode: string;
-	street: string;
-	number: number;
-	complement: string;
-	neighborhood: string;
-	city: string;
-	federatedUnit: string;
+  zipCode: string;
+  street: string;
+  number: number;
+  complement: string;
+  neighborhood: string;
+  city: string;
+  federatedUnit: string;
 }
 export enum PAYMENT_OPTION {
-	CREDIT = "CREDIT",
-	DEBIT = "DEBIT",
-	CASH = "CASH",
+  CREDIT = "CREDIT",
+  DEBIT = "DEBIT",
+  CASH = "CASH",
 }
 
 interface OrderState {
-	coffees: ICoffee[];
-	address: IAddress;
-	paymentMode: PAYMENT_OPTION;
-	isOrderFinished: boolean;
+  coffees: ICoffee[];
+  address: IAddress;
+  paymentMode: PAYMENT_OPTION;
+  isOrderFinished: boolean;
 }
 
 export function orderReducer(state: OrderState, action: any) {
-	switch (action.type) {
-		case ActionTypes.ADD_NEW_COFFEE: {
-			const coffeeIndex = state.coffees.findIndex((coffee) => coffee.id == action.payload.coffee.id);
+  switch (action.type) {
+    case ActionTypes.ADD_NEW_COFFEE: {
+      const coffeeIndex = state.coffees.findIndex(
+        (coffee) => coffee.id == action.payload.coffee.id
+      );
 
-			if (coffeeIndex < 0) {
-				Alert.success("Café adicionado ao carrinho!");
+      if (coffeeIndex < 0) {
+        Alert.success("Café adicionado ao carrinho!");
 
-				const payloadCoffee = action.payload.coffee;
+        const payloadCoffee = action.payload.coffee;
 
-				return produce(state, (draft) => {
-					draft.coffees.push({ ...payloadCoffee, quantity: 1 });
-					setOrderToStorage(draft);
-				});
-			}
-			Alert.error("Café já foi adicionado ao carrinho!");
-			return state;
-		}
-		case ActionTypes.CHANGE_COFFEE_QUANTITY: {
-			const coffeeIndex = state.coffees.findIndex((coffee) => coffee.id == action.payload.coffee.id);
+        return produce(state, (draft) => {
+          draft.coffees.push({ ...payloadCoffee, quantity: 1 });
+          setOrderToStorage(draft);
+        });
+      }
+      Alert.error("Café já foi adicionado ao carrinho!");
+      return state;
+    }
+    case ActionTypes.CHANGE_COFFEE_QUANTITY: {
+      const coffeeIndex = state.coffees.findIndex(
+        (coffee) => coffee.id == action.payload.coffee.id
+      );
 
-			return produce(state, (draft) => {
-				if (coffeeIndex != -1) {
-					draft.coffees[coffeeIndex].quantity = action.payload.quantity;
-					setOrderToStorage(draft);
-				}
-			});
-		}
-		case ActionTypes.REMOVE_COFFEE: {
-			console.log("payload action:" + action.payload.id);
-			return produce(state, (draft) => {
-				draft.coffees = draft.coffees.filter((coffee) => coffee.id !== action.payload.id);
-				setOrderToStorage(draft);
-				Alert.success("Café removido do carrinho!");
-			});
-		}
+      return produce(state, (draft) => {
+        if (coffeeIndex != -1) {
+          draft.coffees[coffeeIndex].quantity = action.payload.quantity;
+          setOrderToStorage(draft);
+        }
+      });
+    }
+    case ActionTypes.REMOVE_COFFEE: {
+      return produce(state, (draft) => {
+        draft.coffees = draft.coffees.filter(
+          (coffee) => coffee.id !== action.payload.id
+        );
+        setOrderToStorage(draft);
+        Alert.success("Café removido do carrinho!");
+      });
+    }
 
-		case ActionTypes.CHANGE_ADDRESS: {
-			return produce(state, (draft) => {
-				draft.address = action.payload.address;
-			});
-		}
+    case ActionTypes.CHANGE_ADDRESS: {
+      return produce(state, (draft) => {
+        draft.address = action.payload.address;
+      });
+    }
 
-		case ActionTypes.CHANGE_PAYMENT_MODE: {
-			return produce(state, (draft) => {
-				draft.paymentMode = action.payload.paymentMode;
-				setOrderToStorage(draft);
-			});
-		}
+    case ActionTypes.CHANGE_PAYMENT_MODE: {
+      return produce(state, (draft) => {
+        draft.paymentMode = action.payload.paymentMode;
+        setOrderToStorage(draft);
+      });
+    }
 
-		case ActionTypes.SET_ORDER_TO_FINISHED: {
-			return produce(state, (draft) => {
-				console.log(state);
-				draft.isOrderFinished = action.payload.isFinished;
-				draft.coffees = [];
-			});
-		}
+    case ActionTypes.SET_ORDER_TO_FINISHED: {
+      return produce(state, (draft) => {
+        draft.isOrderFinished = action.payload.isFinished;
+        draft.coffees = [];
+      });
+    }
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 }
